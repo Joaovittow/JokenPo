7db7627b19d0692658850ced24a406b1335e6339
João Vitor <137925592+JoaoVitor171221@users.noreply.github.com>
2023-08-04 23:18:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log('Hello World!');
+const result = document.querySelector('.result')
+const userScore = document.querySelector('#user-score')
+const machineScore = document.querySelector('#machine-score')
+
+let userScoreNumber = 0
+let machineScoreNumber = 0
+
+const player = (playerChoice) => {
+  
+  playTheGame(playerChoice, playerMachine())
+}
+
+const playerMachine = () => {
+  const choices = ['paper', 'rock', 'scissors']
+  const randomNumber = Math.floor(Math.random() * 3)
+  
+  return choices[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+  console.log("Player: " + human + " Alexa: " + machine)
+  
+  if (human === machine) {
+    result.innerHTML = 'Deu Empate!'
+    
+  } else if(human === 'paper' && machine === 'rock' || human === 'rock' && machine === 'scissors' || human === 'scissors' && machine === 'paper'){
+    result.innerHTML = 'Você Ganhou!'
+    userScoreNumber++
+    userScore.innerHTML = userScoreNumber
+    
+  }else{
+    result.innerHTML = 'Você Perdeu para Alexa!'
+    machineScoreNumber++
+    machineScore.innerHTML = machineScoreNumber
+  }
+}
